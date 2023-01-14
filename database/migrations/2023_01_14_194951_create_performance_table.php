b68e9ec4f2cd72c855599e1d43386c39611d96b6
katrinbeeva <106659227+katrinbeeva@users.noreply.github.com>
2023-01-14 21:54:04 +0200

    create new table and relationship

---
+++ database/migrations/2023_01_14_194951_create_performance_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('performance', function (Blueprint $table) {
+            $table->id();
+            $table->string('name_of_performance');
+            $table->dateTime('performance_date');
+            $table->integer('venue_id');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('performance');
+    }
+};
