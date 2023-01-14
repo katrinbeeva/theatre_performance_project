405d2d051f516079168e2df510561b7e77cdd61e
katrinbeeva <106659227+katrinbeeva@users.noreply.github.com>
2023-01-14 21:58:12 +0200

    create new table (Venue)

--- database/migrations/2023_01_14_194951_create_performance_table.php
+++ database/migrations/2023_01_14_194951_create_performance_table.php
@@ -17,7 +17,6 @@ return new class extends Migration
             $table->id();
             $table->string('name_of_performance');
             $table->dateTime('performance_date');
-            $table->integer('venue_id');
             $table->timestamps();
         });
     }
