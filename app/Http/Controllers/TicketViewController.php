4aec3eee90f76e437787f1f7acb83d63b6c6f983
katrinbeeva <106659227+katrinbeeva@users.noreply.github.com>
2023-01-15 21:33:41 +0200

    update TicketViewController.phpViewController.php

--- app/Http/Controllers/TicketViewController.php
+++ app/Http/Controllers/TicketViewController.php
@@ -3,8 +3,16 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Http\Requests;
+use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\DB;
 
 class TicketViewController extends Controller
 {
-    //
+    public function tickets()
+    {
+        $tickets = DB::table('tickets')->select('price')->get();
+        return (
+        view('tickets_view', ['tickets' => $tickets]));
+    }
 }
