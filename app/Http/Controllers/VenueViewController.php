241449908f8a35b20e2395f9bdfd114540f18758
katrinbeeva <106659227+katrinbeeva@users.noreply.github.com>
2023-01-15 21:36:15 +0200

    update VenueViewController.php

--- app/Http/Controllers/VenueViewController.php
+++ app/Http/Controllers/VenueViewController.php
@@ -3,8 +3,16 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Http\Requests;
+use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\DB;
 
 class VenueViewController extends Controller
 {
-    //
+    public function venues()
+    {
+        $venues = DB::table('venues')->select('name_of_theatre')->get();
+        return (
+        view('venues_view', ['venues' => $venues]));
+    }
 }
